6cbbe69348041e53e97a621294761f5470e47652
minyeoong <minyeong0707@gmail.com>
2023-01-20 00:55:11 +0900

    1157주석처리,1316다른 로직

--- 1157.py
+++ 1157.py
@@ -11,4 +11,20 @@ else:
     special_idx=count_list.index(max(count_list))
     print(uniqueStr[special_idx])
 
+'''
+문자열을 받았을 때 대소문자 구분 없이 가장 빈도수가 높은 알파벳을 대문자로 출력
+난이도 : 브론즈1
+1. 대문자, 소문자 구분
+2. 빈도수가 높은 알파벳이 여러 개일 때 '?'을 출력
+
+string으로 input을 받을 때 upper()을 통해 모두 대문자로 만들어 준다.
+uniqueStr에서 set함수를 이용하여 str 이 갖고 있는 알파벳을 집합으로(중복을 없앤다.)
+uniqueStr의 모든 문자 값에 대하여 반복문을 진행한다.
+str.count(i)을 통해 처음 받은 str문자열에 포함된 모든 문자에 대하여 count를 세어 주고
+count_list에 그 값을 append한다. 
+count_list에는 숫자가 저장 돼 있는데 max숫자가 중복이 될 수 있으므로 
+count_list.count(max(count_list))>1 이면 ?를 출력한다.
+maxr값이 중복이 아니라면 index(max(count_list))를 통해 
+uniqueStr에서 max문자를 갖는 index를 추출한다.
+'''
 
--- 1316.py
+++ 1316.py
@@ -1,12 +1,44 @@
+# N = int(input())
+# cnt = N
+# for i in range(N):
+#     str = input()
+#     for j in range(len(str)-1):
+#         if str[j]==str[j+1]:
+#             pass
+#         else:
+#             if str[j] in str[j+1:]: #str[j+1:]가 또 하나의 문자열임
+#                 cnt-=1
+#                 break
+# print(cnt)
+'''
+그룹 단어란 단어에 존재하는 모든 문자에 대해서,
+각 문자가 연속해서 나타나는 경우만을 말한다
+그룹단어인 문자열을 체크하는 문제이다.
+난이도: 실버5
+            
+            
+'''
+def next_idx_func(i,idx):
+    next_idx=str.index(i,idx+1)
+    return next_idx
+
 N = int(input())
-cnt = N
-for i in range(N):
+cnt =N
+for _ in range(N):
+    flag =False
     str = input()
-    for j in range(len(str)-1):
-        if str[j]==str[j+1]:
+    str_set = list(set(str))
+    for i in str_set:
+        if str.count(i)<2:
             pass
         else:
-            if str[j] in str[j+1:]: #str[j+1:]가 또 하나의 문자열임
-                cnt-=1
-                break
+            next_idx =str.index(i)
+            for _ in range(str.count(i)-1):
+                idx = next_idx
+                next_idx = next_idx_func(i,next_idx)
+                if next_idx-idx!=1:
+                    flag=True
+                    break
+    if flag==True:
+        cnt-=1            
 print(cnt)
